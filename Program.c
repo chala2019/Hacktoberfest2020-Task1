ae6239872337e73fd5adf7b2e2586a0cf6ddcccc
chala2019 <52007759+chala2019@users.noreply.github.com>
2020-10-23 12:52:11 +0530

    Update Program.c
    updated the code snippet

--- Program.c
+++ Program.c
@@ -2,17 +2,21 @@
 
 int main()
 {
-    char name,country,skill;
+    char name[120] ,country[30];
+	char skill,languages;
+	int age;
     printf("Welcome to Hacktoberfest 2020\n");
-    printf("Enter your name : ");//Get User name
-    scanf("%d",&name);
-    printf("Enter your Country of Residance : ");//Get user country
-    scanf("%s",&country);
+    printf("Enter your Full name : ");//Get User name
+    gets(name);
+    printf("Enter your Country  : ");//Get user country
+    gets(country);
     printf("Enter your Age : ");//Get user Age
-    printf("%d",&age);
-    printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
+    scanf("%d",&age);
+    printf("Rate your programmings skills from A-D (A being the highest and D being the lowest) : ");//Get user skill level
     scanf(" %c",&skill);
-    scanf("Thank you %c Happy Hacking",country);//Thank the user
+    printf("what are your expetise areas   : ");//Get user expertise areas
+    scanf(" %c",&languages);
+    printf("Thank you %s Happy Hacking 2020",name);//Thanking the user
 
     return 0;
 }
